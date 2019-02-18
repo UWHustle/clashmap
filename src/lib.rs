cde3ecf161e9eb1aa01b1b74903b82fde308cc0f
gaffneyk <kgaff33@gmail.com>
2019-02-18 16:37:25 -0600

    Created structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::collections::HashMap;
+use std::ptr;
+use std::hash::Hash;
+
+struct Node<K, V> {
+    key: K,
+    value: V,
+    next: *mut Node<K, V>,
+    prev: *mut Node<K, V>
+}
+
+pub struct LinkedHashMap<K, V> {
+    hash_map: HashMap<K, Node<K, V>>,
+    head: *mut Node<K, V>,
+    tail: *mut Node<K, V>
+}
+
+impl<K: Hash + Eq, V> LinkedHashMap<K, V> {
+    pub fn new() -> Self {
+        LinkedHashMap {
+            hash_map: HashMap::new(),
+            head: ptr::null_mut(),
+            tail: ptr::null_mut()
+        }
     }
 }
