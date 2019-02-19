590c718753474ec3ff9721b7f126513fa6313505
gaffneyk <kgaff33@gmail.com>
2019-02-19 11:59:20 -0600

    Renamed Qey

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,15 @@ impl<K: PartialEq, V> PartialEq for Node<K, V> {
 impl<K: Eq, V> Eq for Node<K, V> {}
 
 #[derive(Hash, PartialEq, Eq)]
-struct Qey<Q: ?Sized>(Q);
+struct Key<Q: ?Sized>(Q);
 
-impl<Q: ?Sized> Qey<Q> {
+impl<Q: ?Sized> Key<Q> {
     fn from_ref(q: &Q) -> &Self { unsafe { mem::transmute(q) } }
 }
 
-impl<K, V, Q: ?Sized> Borrow<Qey<Q>> for Box<Node<K, V>> where K: Borrow<Q> {
-    fn borrow(&self) -> &Qey<Q> {
-        Qey::from_ref(self.key.borrow() )
+impl<K, V, Q: ?Sized> Borrow<Key<Q>> for Box<Node<K, V>> where K: Borrow<Q> {
+    fn borrow(&self) -> &Key<Q> {
+        Key::from_ref(self.key.borrow() )
     }
 }
 
@@ -66,7 +66,7 @@ impl<K: Hash + Eq, V> LinkedHashMap<K, V> {
         where K: Borrow<Q>,
               Q: Eq + Hash
     {
-        self.hash_set.get(Qey::from_ref(k)).map(|node| &node.value)
+        self.hash_set.get(Key::from_ref(k)).map(|node| &node.value)
     }
 
     pub fn insert(&mut self, k: K, v: V) {
