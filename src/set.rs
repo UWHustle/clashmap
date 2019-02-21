9c562858f43dbe2821956ea9998b3d59ac1c26c9
gaffneyk <kgaff33@gmail.com>
2019-02-21 11:13:30 -0600

    Added resize

--- src/set.rs
+++ src/set.rs
@@ -3,18 +3,21 @@ use std::collections::hash_map::RandomState;
 use std::sync::{Mutex, RwLock};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::MutexGuard;
+use std::sync::RwLockWriteGuard;
+use std::f32::MIN;
+use std::mem;
 
 const MIN_CAPACITY: usize = 2^4;
 const MAX_LOAD_NUM: usize = 1;
 const MAX_LOAD_DEN: usize = 2;
 
 struct Bucket<T> {
-    pub value: Mutex<Option<T>>
+    value: Option<T>
 }
 
 struct Table<T> {
     hash_builder: RandomState,
-    buckets: Vec<Bucket<T>>
+    buckets: Vec<RwLock<Bucket<T>>>
 }
 
 pub struct ConcurrentHashSet<T> {
@@ -23,30 +26,30 @@ pub struct ConcurrentHashSet<T> {
 }
 
 impl<T> Bucket<T> {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Bucket {
-            value: Mutex::new(None)
+            value: None
         }
     }
 }
 
 impl<T> Table<T> {
-    pub fn new() -> Self {
+    fn with_capacity(capacity: usize) -> Self {
         Table {
             hash_builder: RandomState::new(),
-            buckets: (0..MIN_CAPACITY).map(|_| Bucket::new()).collect()
+            buckets: (0..capacity).map(|_| RwLock::new(Bucket::new())).collect()
         }
     }
 
-    pub fn find(&self, value: &T) -> MutexGuard<Option<T>>
+    fn find_mut(&self, value: &T) -> RwLockWriteGuard<Bucket<T>>
         where T: Hash + Eq
     {
         let hash = self.hash(value);
         let mut i = hash % self.buckets.len();
         loop {
-            let value_guard = self.buckets[i].value.lock().unwrap();
-            if (*value_guard).is_none() || (*value_guard).as_ref().unwrap() == value {
-                break value_guard;
+            let bucket_guard = self.buckets[i].write().unwrap();
+            if bucket_guard.value.is_none() || bucket_guard.value.as_ref().unwrap() == value {
+                break bucket_guard;
             }
             i = (i + 1) % self.buckets.len();
         }
@@ -64,7 +67,7 @@ impl<T> Table<T> {
 impl<T> ConcurrentHashSet<T> {
     pub fn new() -> Self {
         ConcurrentHashSet {
-            table: RwLock::new(Table::new()),
+            table: RwLock::new(Table::with_capacity(MIN_CAPACITY)),
             size: AtomicUsize::new(0)
         }
     }
@@ -73,7 +76,9 @@ impl<T> ConcurrentHashSet<T> {
         self.table.read().unwrap().buckets.len() * MAX_LOAD_NUM / MAX_LOAD_DEN
     }
 
-    pub fn reserve(&self, additional: usize) {
+    pub fn reserve(&self, additional: usize)
+        where T: Hash + Eq
+    {
         if self.capacity() - self.len() < additional {
             let raw_capacity = (self.len() + additional) * MAX_LOAD_DEN / MAX_LOAD_NUM;
             self.resize(raw_capacity);
@@ -93,23 +98,35 @@ impl<T> ConcurrentHashSet<T> {
     {
         self.reserve(1);
         let table_guard = self.table.read().unwrap();
-        let mut value_guard = table_guard.find(&value);
-        let replaced_value = (*value_guard).replace(value);
+        let mut bucket_guard = table_guard.find_mut(&value);
+        let replaced_value = bucket_guard.value.replace(value);
         if replaced_value.is_some() {
             self.size.fetch_add(1, Ordering::Relaxed);
         }
         replaced_value
     }
 
-    fn resize(&self, new_raw_capacity: usize) {
-//        let mut buckets_guard = self.buckets.write().unwrap();
-//        if buckets_guard.len() < new_raw_capacity {
-////            let new_buckets = (0..new_raw_capacity).map(|_| Bucket::new()).collect();
-////            for bucket in *buckets_guard {
-////                if let Some(value) = *bucket.value.lock().unwrap() {
-////
-////                }
-////            }
+    fn resize(&self, new_raw_capacity: usize)
+        where T: Hash + Eq
+    {
+        let mut table_guard = self.table.write().unwrap();
+        if table_guard.buckets.len() < new_raw_capacity {
+            let old_table = mem::replace(&mut *table_guard, Table::with_capacity(new_raw_capacity));
+            for bucket in old_table.buckets {
+                if let Some(value) = bucket.into_inner().unwrap().value {
+                    let mut bucket_guard = table_guard.find_mut(&value);
+                    bucket_guard.value.replace(value);
+                }
+            }
+        }
+
+//        if self.buckets.len() < new_raw_capacity {
+//            let new_buckets = (0..new_raw_capacity).map(|_| Bucket::new()).collect();
+//            for bucket in self.buckets {
+//                if let Some(value) = *bucket.value.lock().unwrap() {
+//
+//                }
+//            }
 //        }
     }
 }
