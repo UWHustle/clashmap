92869fa4c3c356e329e81f5bbfd81f8fe8ba9f7e
gaffneyk <kgaff33@gmail.com>
2019-02-27 17:21:36 -0600

    Changed bucket to record

--- src/map.rs
+++ src/map.rs
@@ -12,17 +12,17 @@ const MAX_LOAD_NUM: usize = 1;
 const MAX_LOAD_DEN: usize = 2;
 
 pub struct ValueGuard<'a, K, V: 'a>(
-    OwningRef<OwningHandle<RwLockReadGuard<'a, Table<K, V>>, RwLockReadGuard<'a, Option<Bucket<K, V>>>>, V>
+    OwningRef<OwningHandle<RwLockReadGuard<'a, Table<K, V>>, RwLockReadGuard<'a, Option<Record<K, V>>>>, V>
 );
 
-pub struct Bucket<K, V> {
+pub struct Record<K, V> {
     key: K,
     value: V
 }
 
 struct Table<K, V> {
     hash_builder: RandomState,
-    buckets: Vec<RwLock<Option<Bucket<K, V>>>>
+    records: Vec<RwLock<Option<Record<K, V>>>>
 }
 
 pub struct ConcurrentHashMap<K, V> {
@@ -52,9 +52,9 @@ impl<'a, K, V> PartialEq<V> for ValueGuard<'a, K, V>
     }
 }
 
-impl<K, V> Bucket<K, V> {
+impl<K, V> Record<K, V> {
     fn new(key: K, value: V) -> Self {
-        Bucket {
+        Record {
             key,
             value
         }
@@ -65,44 +65,44 @@ impl<K, V> Table<K, V> {
     fn with_capacity(capacity: usize) -> Self {
         Table {
             hash_builder: RandomState::new(),
-            buckets: (0..capacity).map(|_| RwLock::new(None)).collect()
+            records: (0..capacity).map(|_| RwLock::new(None)).collect()
         }
     }
 
-    fn find<Q: ?Sized>(&self, key: &Q) -> (usize, &RwLock<Option<Bucket<K, V>>>)
+    fn find<Q: ?Sized>(&self, key: &Q) -> (usize, &RwLock<Option<Record<K, V>>>)
         where K: Borrow<Q>,
               Q: Hash + Eq
     {
         let hash = self.hash(key);
-        let mut i = hash % self.buckets.len();
+        let mut i = hash % self.records.len();
         loop {
-            let bucket = &self.buckets[i];
-            let bucket_guard = self.buckets[i].read().unwrap();
-            if bucket_guard.is_none() || key.eq(bucket_guard.as_ref().unwrap().key.borrow()) {
-                break (i, bucket);
+            let record = &self.records[i];
+            let record_guard = self.records[i].read().unwrap();
+            if record_guard.is_none() || key.eq(record_guard.as_ref().unwrap().key.borrow()) {
+                break (i, record);
             }
-            i = (i + 1) % self.buckets.len();
+            i = (i + 1) % self.records.len();
         }
     }
 
-    fn take_shift<Q: ?Sized>(&self, key: &Q) -> Option<Bucket<K, V>>
+    fn take_shift<Q: ?Sized>(&self, key: &Q) -> Option<Record<K, V>>
         where K: Borrow<Q>,
               Q: Hash + Eq
     {
-        let (i, bucket_lock) = self.find(key);
-        let mut bucket_guard = bucket_lock.write().unwrap();
+        let (i, record_lock) = self.find(key);
+        let mut record_guard = record_lock.write().unwrap();
 
-        bucket_guard.take().map(|bucket| {
+        record_guard.take().map(|record| {
             let mut swap_index = i;
-            while self.buckets[(swap_index + 1) % self.buckets.len()].read().unwrap().is_some() {
+            while self.records[(swap_index + 1) % self.records.len()].read().unwrap().is_some() {
                 swap_index += 1;
             }
 
             if swap_index != i {
-                let mut swap_bucket_guard = self.buckets[swap_index % self.buckets.len()].write().unwrap();
-                mem::swap(&mut bucket_guard, &mut swap_bucket_guard);
+                let mut swap_record_guard = self.records[swap_index % self.records.len()].write().unwrap();
+                mem::swap(&mut record_guard, &mut swap_record_guard);
             }
-            bucket
+            record
         })
     }
 
@@ -131,7 +131,7 @@ impl<K, V> ConcurrentHashMap<K, V>
     }
 
     pub fn capacity(&self) -> usize {
-        self.table.read().unwrap().buckets.len() * MAX_LOAD_NUM / MAX_LOAD_DEN
+        self.table.read().unwrap().records.len() * MAX_LOAD_NUM / MAX_LOAD_DEN
     }
 
     pub fn reserve(&self, additional: usize) {
@@ -159,12 +159,12 @@ impl<K, V> ConcurrentHashMap<K, V>
         where K: Borrow<Q>,
               Q: Hash + Eq
     {
-        let bucket_handle = OwningHandle::new_with_fn(
+        let record_handle = OwningHandle::new_with_fn(
             self.table.read().unwrap(),
             |t| unsafe {&*t}.find(key).1.read().unwrap());
 
-        match &*bucket_handle {
-            Some(_) => Some(ValueGuard(OwningRef::new(bucket_handle).map(|b| &b.as_ref().unwrap().value))),
+        match &*record_handle {
+            Some(_) => Some(ValueGuard(OwningRef::new(record_handle).map(|b| &b.as_ref().unwrap().value))),
             None => None
         }
     }
@@ -174,9 +174,9 @@ impl<K, V> ConcurrentHashMap<K, V>
     {
         self.reserve(1);
         let table_guard = self.table.read().unwrap();
-        let mut bucket_guard = table_guard.find(&key).1.write().unwrap();
-        let replaced_bucket = bucket_guard.replace(Bucket::new(key, value));
-        replaced_bucket
+        let mut record_guard = table_guard.find(&key).1.write().unwrap();
+        let replaced_record = record_guard.replace(Record::new(key, value));
+        replaced_record
             .map(|b| b.value)
             .or_else(|| {
                 self.size.fetch_add(1, Ordering::Relaxed);
@@ -195,12 +195,12 @@ impl<K, V> ConcurrentHashMap<K, V>
         where K: Hash
     {
         let mut table_guard = self.table.write().unwrap();
-        if table_guard.buckets.len() < new_raw_capacity {
+        if table_guard.records.len() < new_raw_capacity {
             let old_table = mem::replace(&mut *table_guard, Table::with_capacity(new_raw_capacity));
-            for bucket in old_table.buckets {
-                if let Some(b) = bucket.into_inner().unwrap().take() {
-                    let mut bucket_guard = table_guard.find(&b.key).1.write().unwrap();
-                    bucket_guard.replace(b);
+            for record in old_table.records {
+                if let Some(b) = record.into_inner().unwrap().take() {
+                    let mut record_guard = table_guard.find(&b.key).1.write().unwrap();
+                    record_guard.replace(b);
                 }
             }
         }
