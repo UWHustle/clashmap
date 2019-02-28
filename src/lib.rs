3fa559fbb841163d451c4187c7f2315de246c393
gaffneyk <kgaff33@gmail.com>
2019-02-28 16:34:03 -0600

    Reverted to non-concurrent implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,172 @@
-pub mod linked;
-pub mod map;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use std::mem;
+use std::borrow::Borrow;
+use std::ptr;
+use std::ops::Index;
+use std::marker::PhantomData;
 
-pub use map::ClashMap;
+struct Node<K, V> {
+    key: K,
+    value: V,
+    next: *mut Node<K, V>,
+    prev: *mut Node<K, V>
+}
+
+pub struct OrderedHashMap<K, V> {
+    hash_set: HashSet<Box<Node<K, V>>>,
+    head: *mut Node<K, V>,
+    tail: *mut Node<K, V>
+}
+
+impl<K: Hash, V> Hash for Node<K, V> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.key.hash(state)
+    }
+}
+
+impl<K: PartialEq, V> PartialEq for Node<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.key.eq(&other.key)
+    }
+}
+
+impl<K: Eq, V> Eq for Node<K, V> {}
+
+#[derive(Hash, PartialEq, Eq)]
+struct Key<Q: ?Sized>(Q);
+
+impl<Q: ?Sized> Key<Q> {
+    fn from_ref(q: &Q) -> &Self { unsafe { mem::transmute(q) } }
+}
+
+impl<K, V, Q: ?Sized> Borrow<Key<Q>> for Box<Node<K, V>> where K: Borrow<Q> {
+    fn borrow(&self) -> &Key<Q> {
+        Key::from_ref(self.key.borrow() )
+    }
+}
+
+impl<K, V> Node<K, V> {
+    pub fn new(k: K, v: V) -> Self {
+        Node {
+            key: k,
+            value: v,
+            next: ptr::null_mut(),
+            prev: ptr::null_mut()
+        }
+    }
+}
+
+impl<K: Hash + Eq, V> OrderedHashMap<K, V> {
+    pub fn new() -> Self {
+        OrderedHashMap {
+            hash_set: HashSet::new(),
+            head: ptr::null_mut(),
+            tail: ptr::null_mut()
+        }
+    }
+
+    pub fn iter(&self) -> Iter<K, V> {
+        Iter {
+            head: self.head,
+            remaining: self.len(),
+            marker: PhantomData
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.hash_set.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.hash_set.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.hash_set.clear();
+        self.head = ptr::null_mut();
+        self.tail = ptr::null_mut();
+    }
+
+    pub fn get<Q: ?Sized>(&self, k: &Q) -> Option<&V>
+        where K: Borrow<Q>,
+              Q: Eq + Hash
+    {
+        self.hash_set.get(Key::from_ref(k)).map(|node| &node.value)
+    }
+
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        let mut node = Box::new(Node::new(k, v));
+        let raw_node: *mut _ = &mut *node;
+        if self.tail.is_null() {
+            self.head = raw_node;
+            self.tail = raw_node;
+        } else {
+            unsafe {
+                node.prev = self.tail;
+                (*self.tail).next = raw_node;
+                self.tail = raw_node
+            };
+        }
+        self.hash_set.replace(node).map(|node| node.value)
+    }
+
+    pub fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
+        where K: Borrow<Q>,
+              Q: Eq + Hash
+    {
+        self.hash_set.take(Key::from_ref(k)).map(|node| {
+            if node.prev.is_null() {
+                self.head = node.next
+            } else {
+                unsafe {
+                    (*node.prev).next = node.next
+                }
+            }
+
+            if node.next.is_null() {
+                self.tail = node.prev
+            } else {
+                unsafe {
+                    (*node.next).prev = node.prev
+                }
+            }
+
+            node.value
+        })
+    }
+}
+
+impl<'a, K, V, Q: ?Sized> Index<&'a Q> for OrderedHashMap<K, V>
+    where K: Eq + Hash + Borrow<Q>,
+          Q: Eq + Hash
+{
+    type Output = V;
+
+    fn index(&self, key: &Q) -> &V {
+        self.get(key).expect("no entry found for key")
+    }
+}
+
+pub struct Iter<'a, K: 'a, V: 'a> {
+    head: *const Node<K, V>,
+    remaining: usize,
+    marker: PhantomData<(&'a K, &'a V)>
+}
+
+impl<'a, K, V> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<(&'a K, &'a V)> {
+        if self.head.is_null() {
+            None
+        } else {
+            self.remaining -= 1;
+            unsafe {
+                let result = Some((&(*self.head).key, &(*self.head).value));
+                self.head = (*self.head).next;
+                result
+            }
+        }
+    }
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,26 +1,26 @@
-extern crate clashmap;
+extern crate omap;
 
 #[cfg(test)]
 mod tests {
 
-    use clashmap::ClashMap;
+    use omap::OrderedHashMap;
 
-//    #[test]
-//    fn iter() {
-//        let mut map = ClashMap::new();
-//        map.insert("a", 1);
-//        map.insert("b", 2);
-//        map.insert("c", 3);
-//
-//        let mut iter = map.iter();
-//        assert_eq!(iter.next(), Some((&"a", &1)));
-//        assert_eq!(iter.next(), Some((&"b", &2)));
-//        assert_eq!(iter.next(), Some((&"c", &3)));
-//    }
-//
+    #[test]
+    fn iter() {
+        let mut map = OrderedHashMap::new();
+        map.insert("a", 1);
+        map.insert("b", 2);
+        map.insert("c", 3);
+
+        let mut iter = map.iter();
+        assert_eq!(iter.next(), Some((&"a", &1)));
+        assert_eq!(iter.next(), Some((&"b", &2)));
+        assert_eq!(iter.next(), Some((&"c", &3)));
+    }
+
     #[test]
     fn len() {
-        let a = ClashMap::new();
+        let mut a = OrderedHashMap::new();
         assert_eq!(a.len(), 0);
         a.insert(1, "a");
         assert_eq!(a.len(), 1);
@@ -28,7 +28,7 @@ mod tests {
 
     #[test]
     fn is_empty() {
-        let a = ClashMap::new();
+        let mut a = OrderedHashMap::new();
         assert!(a.is_empty());
         a.insert(1, "a");
         assert!(!a.is_empty());
@@ -36,36 +36,36 @@ mod tests {
 
     #[test]
     fn clear() {
-        let a = ClashMap::new();
+        let mut a = OrderedHashMap::new();
         a.insert(1, "a");
         a.clear();
         assert!(a.is_empty());
     }
-//
-//    #[test]
-//    fn get() {
-//        let map = ClashMap::new();
-//        map.insert(1, "a");
-//        assert_eq!(map.get(&1).unwrap(), &"a");
-//        assert!(map.get(&2).is_none());
-//    }
-//
-//    #[test]
-//    fn insert() {
-//        let map = ClashMap::new();
-//        assert_eq!(map.insert(37, "a"), None);
-//        assert_eq!(map.is_empty(), false);
-//
-//        map.insert(37, "b");
-//        assert_eq!(map.insert(37, "c"), Some("b"));
-//        assert_eq!(map.get(&37).unwrap(), "c");
-//    }
-//
-//    #[test]
-//    fn remove() {
-//        let map = ClashMap::new();
-//        map.insert(1, "a");
-//        assert_eq!(map.remove(&1), Some("a"));
-//        assert_eq!(map.remove(&1), None);
-//    }
+
+    #[test]
+    fn get() {
+        let mut map = OrderedHashMap::new();
+        map.insert(1, "a");
+        assert_eq!(map.get(&1), Some(&"a"));
+        assert_eq!(map.get(&2), None);
+    }
+
+    #[test]
+    fn insert() {
+        let mut map = OrderedHashMap::new();
+        assert_eq!(map.insert(37, "a"), None);
+        assert_eq!(map.is_empty(), false);
+
+        map.insert(37, "b");
+        assert_eq!(map.insert(37, "c"), Some("b"));
+        assert_eq!(map[&37], "c");
+    }
+
+    #[test]
+    fn remove() {
+        let mut map = OrderedHashMap::new();
+        map.insert(1, "a");
+        assert_eq!(map.remove(&1), Some("a"));
+        assert_eq!(map.remove(&1), None);
+    }
 }
